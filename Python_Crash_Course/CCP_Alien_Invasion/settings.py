00bd191a98183a5f4bd2e27ae50f8aecb2b6c06d
leaf-supreme <leaf-supreme@gmail.com>
2024-09-05 23:27:39 -0700

    Modify ship movement and speed

--- Python_Crash_Course/CCP_Alien_Invasion/settings.py
+++ Python_Crash_Course/CCP_Alien_Invasion/settings.py
@@ -9,4 +9,5 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bkgnd_color = (45,53,94)   #background RGB
-        
+        # Ship settings 
+        self.ship_speed = 10.5 #1.5 pixels but rect uses only integers 
--- Python_Crash_Course/CCP_Alien_Invasion/ship.py
+++ Python_Crash_Course/CCP_Alien_Invasion/ship.py
@@ -1,5 +1,6 @@
 # Class for ship in Alien Invasion 
 import pygame
+from settings import Settings
 
 class Ship:
     """Class for ship in alien invasions"""
@@ -8,6 +9,7 @@ class Ship:
         """Intialize Ship class"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
         
         # load the ship bmp from image folder
         self.image = pygame.image.load('images/shipp.bmp')
@@ -15,6 +17,10 @@ class Ship:
         
         # start a new ship at bottom mid of screen
         self.rect.midbottom = self.screen_rect.midbottom
+        
+        # Store a float for ships exact hotizontal position 
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
         
         # movement flag; start with a ship that isnt moving
         self.moving_right = False
@@ -24,14 +30,20 @@ class Ship:
         
     def update(self):
         """Update ship position based on movement flag"""
-        if self.moving_right:
-            self.rect.x +=1
-        if self.moving_left:
-            self.rect.x -=1
+        #update ships position x value not rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
         if self.moving_foreward:
-            self.rect.y -=1
+            self.y -= self.settings.ship_speed
         if self.moving_backward:
-            self.rect.y +=1
+            self.y += self.settings.ship_speed
+            
+        # Update rect object from self.x
+        self.rect.x = self.x
+        self.rect.y = self.y
+            
             
     def blitme(self):
         """Draw shop at its current location"""
